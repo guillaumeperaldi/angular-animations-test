fbdf72fff2473cc1faad73a91fd91938636ccbe3
guillaumeperaldi <gmperaldi@gmail.com>
2020-10-14 10:33:36 +0200

    Components dans composants

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HelloComponent } from './components/hello.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, BrowserAnimationsModule ],
