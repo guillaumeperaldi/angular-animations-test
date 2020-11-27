d921c234f9800f1e41974353c5234f7b6b5fcbac
guillaumeperaldi <gmperaldi@gmail.com>
2020-11-27 17:20:22 +0100

    2 files modified, 5 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,23 +5,12 @@ import {
   HAMMER_GESTURE_CONFIG
 } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
-import { RouterModule } from "@angular/router";
 import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { Page1Component } from "./components/page1/page1.component";
-import { Page2Component } from "./components/page2/page2.component";
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    FormsModule,
-    BrowserAnimationsModule,
-    RouterModule.forRoot([
-      { path: "./components/page1", component: Page1Component },
-      { path: "./components/page2", component: Page2Component }
-    ])
-  ],
-  declarations: [AppComponent, Page1Component, Page2Component],
+  imports: [BrowserModule, FormsModule, BrowserAnimationsModule],
+  declarations: [AppComponent],
   bootstrap: [AppComponent],
   providers: [
     {
