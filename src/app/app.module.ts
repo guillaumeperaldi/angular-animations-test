cf7217ca2a42896c25cf6977fcd33eb0ce7676b0
guillaumeperaldi <60196237+guillaumeperaldi@users.noreply.github.com>
2023-10-16 12:27:08 +0200

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,24 @@
-import { NgModule } from "@angular/core";
+import { NgModule } from '@angular/core';
 import {
   BrowserModule,
   HammerGestureConfig,
-  HAMMER_GESTURE_CONFIG
-} from "@angular/platform-browser";
-import { FormsModule } from "@angular/forms";
-import { RouterModule } from "@angular/router";
-import { AppComponent } from "./app.component";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { Page1Component } from "./components/page1/page1.component";
-import { Page2Component } from "./components/page2/page2.component";
+  HAMMER_GESTURE_CONFIG,
+} from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Page1Component } from './components/page1/page1.component';
+import { Page2Component } from './components/page2/page2.component';
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    FormsModule,
-    BrowserAnimationsModule,
-    RouterModule.forRoot([
-      { path: "./components/page1", component: Page1Component },
-      { path: "./components/page2", component: Page2Component }
-    ])
-  ],
+  imports: [BrowserModule, FormsModule, BrowserAnimationsModule],
   declarations: [AppComponent, Page1Component, Page2Component],
   bootstrap: [AppComponent],
   providers: [
     {
       provide: HAMMER_GESTURE_CONFIG,
-      useClass: HammerGestureConfig
-    }
-  ]
+      useClass: HammerGestureConfig,
+    },
+  ],
 })
 export class AppModule {}
